d5f9ae0f5b84a3e7d8a5b84b9666abdc78815b9f
Rasim <rasim.mehtijev@insly.com>
2024-08-08 11:21:38 +0300

    fix mergeSchemas to expect that schema property can be boolean value

--- packages/utils/src/mergeSchemas.ts
+++ packages/utils/src/mergeSchemas.ts
@@ -18,7 +18,7 @@ export default function mergeSchemas(obj1: GenericObjectType, obj2: GenericObjec
   return Object.keys(obj2).reduce((acc, key) => {
     const left = obj1 ? obj1[key] : {},
       right = obj2[key];
-    if (obj1 && key in obj1 && isObject(right)) {
+    if (isObject(obj1) && isObject(right) && key in obj1) {
       acc[key] = mergeSchemas(left, right);
     } else if (
       obj1 &&
